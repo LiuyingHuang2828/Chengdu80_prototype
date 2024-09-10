a9d5703f509ca6e0e787382fa9ead3952c0fd3da
JyTey2004 <tey.jia.ye@gmail.com>
2024-09-10 23:27:17 +0800

    Added things not saved

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from news_fetcher.fetch_news import NewsFetcher
+import os
+from dotenv import load_dotenv
 
-# Replace 'Tesla' with the desired company name
-company_name = 'Tesla'
+# Load environment variables from .env file
+load_dotenv()
 
-# Instantiate the NewsFetcher
+# Define the company name you want to search for
+company_name = 'Tesla'  # Replace with the desired company name
+
+# Instantiate the NewsFetcher with the company name
 fetcher = NewsFetcher(company_name)
 
 # Fetch and save news articles for all defined risk types
-fetcher.fetch_all_risks()
+fetcher.fetch_all_risks(output_dir='news_data_fetched')
